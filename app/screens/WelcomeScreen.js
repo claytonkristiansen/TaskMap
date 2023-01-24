4ce4e6c55b7cc608ff47a79add62ff507b98a57f
Clayton Kristiansen <clayton.kristiansen@hotmail.com>
2023-01-23 22:02:19 -0600

    Configured for Powershell and added grid

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { ImageBackground, View, StyleSheet, Button, Alert, Text } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 
 function WelcomeScreen(props) {
 
     const handlePress = () => console.log("handlePress called")
 
     return (
-        <ImageBackground
-            style={styles.background}
-            source={require("../assets/beachbackground.png")}
-        >
-            <View style={styles.loginButtonView}>
-                <Text onPress={handlePress}>Hello</Text>
-            </View>
-        </ImageBackground>
+        // <ImageBackground
+        //     style={styles.background}
+        //     source={require("../assets/beachbackground.png")}
+        // >
+            
+        //     <View style={styles.loginButtonView}>
+        //         <Text onPress={handlePress}>Hello</Text>
+        //     </View>
+        // </ImageBackground>
+        <FlatGrid
+                itemDimension={200}
+                spacing={10}
+                data={[1,2,3,4,5,6]}
+                renderItem={({ item }) => (
+                    <View style={styles.gridView}>
+                        <Text>{item}</Text>
+                    </View>
+                    )}
+                />
     );
 }
 
@@ -28,6 +40,9 @@ const styles = StyleSheet.create({
         height: 70,
         //backgroundColor: '#fc5f65'
     },
+    gridView: {
+        backgroundColor: '#500000'
+    }
 })
 
 export default WelcomeScreen;
